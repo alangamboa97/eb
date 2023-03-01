445829495b72f0fed2696e7ca9fef22710b81d5b
Alan Gamboa <alangam97@gmail.com>
2023-02-28 18:51:44 -0600

    signout

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ebase35f92a6b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports'; 
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import { I18n } from 'aws-amplify';
+Amplify.configure(config);
+
+I18n.setLanguage('es');
+const dict = {
+ 
+  es: {
+    'Sign In': 'Registrarse',
+    'Create Account': 'Regístrate',
+    'Enter your Email': 'Ingresa tu Email',
+    'Password': 'Contraseña',
+    'Enter your Password':'Ingresa tu Contraseña',
+    
+
+  }
+};
+
+I18n.putVocabularies(dict);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Holi
+      
+        
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
