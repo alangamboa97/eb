8ed1deb6f1a415e0e64dfbb38f230a4f37dc4a71
Alan Gamboa <alangam97@gmail.com>
2023-03-25 12:52:30 -0600

    s3

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,37 @@
-import { Amplify, API } from "aws-amplify";
+import { Amplify, API, graphqlOperation } from "aws-amplify";
 import config from './aws-exports';
-
-
 import { useAuthenticator,Authenticator} from "@aws-amplify/ui-react";
+import { listConductors } from "./graphql/queries";
+import { useEffect, useState } from "react";
 
 
+
+Amplify.configure(config);
 export default function Home() {
   const { route } = useAuthenticator((context) => [context.route]);
   const message =
     route === 'authenticated' ? 'FIRST PROTECTED ROUTE!' : 'Loading...';
 
+    const [conductores, setConductores] = useState([]);
+    
+    useEffect(()=>{
+      fetchCoductores()
+    },[])
+
+    //lee los conductores de la base de datos
+    const fetchCoductores = async() =>{
+      try{
+        const conductorData = await API.graphql(graphqlOperation(listConductors));
+        const conductorNombre = conductorData.data.listConductors.items;
+        console.log(conductorNombre);
+        setConductores(conductorNombre);
+
+      }
+      catch (error){
+        console.log('error leyendo datos', error)
+
+      }
+    }
     
 
     return (
@@ -18,7 +40,13 @@ export default function Home() {
           <main>
             <h1>Bienvenido a Home</h1>
            
-
+            <div className="lista_conductores">
+              {
+                conductores.map(conductor =>{
+                  
+                })
+              }
+            </div>
 
             <button onClick={signOut}>Sign out</button>
           </main>
