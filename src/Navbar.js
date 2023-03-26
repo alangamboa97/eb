b2d8fa2cc0bf6240ef911649f691bcb61c8a4ffe
Alan Gamboa <alangam97@gmail.com>
2023-03-25 18:27:01 -0600

    denuevo

---
+++ src/Navbar.js
@@ -0,0 +1,61 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
+
+
+/*
+      <nav>
+        <Button onClick={() => navigate('/')}>Home</Button>
+        <Button onClick={() => navigate('/conductor')}>
+          Conductor
+        </Button>
+        <Button onClick={() => navigate('/Incidencia')}>
+         Incidencia
+        </Button>
+        <Button onClick={() => navigate('/Navbar')}>
+         Navbar
+        </Button>
+        {route !== 'authenticated' ? (
+          <Button onClick={() => navigate('/login')}>Login</Button>
+        ) : (
+          <Button onClick={() => logOut()}>Logout</Button>
+        )}
+      </nav>
+      <Heading level={1}>Login</Heading>
+      <View>
+        {route === 'authenticated' ? 'Has ingresado' : 'Porfavor inicia sesión'}
+      </View>
+
+      <Outlet />
+    </>
